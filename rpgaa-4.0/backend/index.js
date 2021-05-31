2863d796022ccc09fa15144e28d8f9d9f331ecc0
William Wassmann <wmwassmann@hotmail.com>
2021-05-31 13:30:38 -0700

    first commit

--- rpgaa-4.0/backend/index.js
+++ rpgaa-4.0/backend/index.js
@@ -2,16 +2,32 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import cors from 'cors';
+
 
 import usersRouter from "./routes/users.js";
 import dialogRouter from "./routes/dialog.js";
 import narrationRouter from "./routes/narration.js";
 
+import dotenv from 'dotenv'
+
+import connectDB from './config/db.js';
+
+connectDB()
 
 const app = express();
-const connection = mongoose.connection;
+// const connection = mongoose.connection; 
+
+const result = dotenv.config()
+
+if (result.error) {
+  throw result.error
+}
+
+
+console.log(process.env.ATLAS_URI)
+console.log(result.parsed)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -22,18 +38,18 @@ app.use("/users", usersRouter);
 app.use("/dialogs", dialogRouter);
 app.use("/narration", narrationRouter);
 
-const ATLAS_URI = 'mongodb+srv://wmwassmann:thunderThunderTHUNDERCATSh0000@rpgaa.59ktj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const ATLAS_URI = process.env.ATLAS_URI
+const PORT = process.env.PORT
 
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully!');
-})
+// connection.once('open', () => {
+//     console.log('MongoDB database connection established successfully!');
+// })
 
-mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+// mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
 
 
 
---
+++ rpgaa-4.0/backend/utils/importData.js
@@ -0,0 +1,14 @@
+// import dotenv from 'dotenv'
+// import fs from 'fs'
+
+// const result = dotenv.config({path: './.env'})
+
+// // import answer from '../models/answer.model'
+// import dialog from '../models/dialog.model'
+// import narration from '../models/narration.model'
+// import users from '../models/users.model'
+// import connectDB from '../config/db'
+
+// connectDB()
+
+// const users = JSON.parse(fs.readFileSync(`${_dirname}/dialogs`))
